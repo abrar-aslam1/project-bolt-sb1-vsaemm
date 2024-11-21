72fb2d5aa6ca57aa08d255bb685d05d9427c6083
Abrar Aslam <abraraslam139@gmail.com>
2024-11-20 22:37:00 -0600

    db changes

--- project/lib/supabase.ts
+++ project/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://jbohbzkwmhjhpezbmmuv.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY || ''
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impib2hiemt3bWhqaHBlemJtbXV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxNTY1ODMsImV4cCI6MjA0NzczMjU4M30.uJw070pcoZWfV40TDxQm4ZrSi9tDzJT3w2iGiOoL3PE'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey,
+  {
+    auth: {
+      persistSession: false
+    }
+  }
+);
